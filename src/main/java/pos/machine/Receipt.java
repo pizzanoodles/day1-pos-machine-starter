ca70191511c1be93b3d4e19e44707ea6e8afe935
Jens Jovellano <jensjovellano@gmail.com>
2023-08-15 18:17:30 +0800

    feat: added ReceiptItem and Receipt classes

---
+++ src/main/java/pos/machine/Receipt.java
@@ -0,0 +1,8 @@
+package pos.machine;
+
+import java.util.List;
+
+public class Receipt {
+    private List<ReceiptItem> receiptItems;
+    private int totalPrice;
+}
---
+++ src/main/java/pos/machine/ReceiptItem.java
@@ -0,0 +1,8 @@
+package pos.machine;
+
+public class ReceiptItem {
+    private String name;
+    private String quantity;
+    private int unitPrice;
+    private int subTotal;
+}
