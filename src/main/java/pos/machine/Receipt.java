7e9642f68845df26ff5c7136b684204fa4373adc
Jens Jovellano <jensjovellano@gmail.com>
2023-08-15 19:33:33 +0800

    feat: added setters and getters to the classes

--- src/main/java/pos/machine/Receipt.java
+++ src/main/java/pos/machine/Receipt.java
@@ -5,4 +5,20 @@ import java.util.List;
 public class Receipt {
     private List<ReceiptItem> receiptItems;
     private int totalPrice;
+
+    public void setReceiptItems(List<ReceiptItem> receiptItems) {
+        this.receiptItems = receiptItems;
+    }
+
+    public void setTotalPrice(int totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+
+    public int getTotalPrice() {
+        return totalPrice;
+    }
+
+    public List<ReceiptItem> getReceiptItems() {
+        return receiptItems;
+    }
 }
--- src/main/java/pos/machine/ReceiptItem.java
+++ src/main/java/pos/machine/ReceiptItem.java
@@ -2,7 +2,39 @@ package pos.machine;
 
 public class ReceiptItem {
     private String name;
-    private String quantity;
+    private int quantity;
     private int unitPrice;
     private int subTotal;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public void setUnitPrice(int unitPrice) {
+        this.unitPrice = unitPrice;
+    }
+
+    public void setSubTotal(int subTotal) {
+        this.subTotal = subTotal;
+    }
+
+    public int getUnitPrice() {
+        return unitPrice;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public int getSubTotal() {
+        return subTotal;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
